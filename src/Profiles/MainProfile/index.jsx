ef4fb566cb84a9e047bb0d11a90bd1114c6d3c48
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2023-03-05 17:55:26 -0400

    pagina principal

--- src/Profiles/MainProfile/index.jsx
+++ src/Profiles/MainProfile/index.jsx
@@ -5,10 +5,11 @@ export function MainProfile() {
     <Profile
       capaUrl="https://scontent.foal2-1.fna.fbcdn.net/v/t1.18169-9/22281875_1182115135221568_3618775328753707118_n.png?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFGaTBiH3CHC2aZ9XhHMZtYcT0Onzx6ilZxPQ6fPHqKVh7JFViYDx9gF44uB-kiDF5JTBd1kB9WBe6O8evKP2bp&_nc_ohc=9sGGfP9_XAEAX9bWy1e&_nc_ht=scontent.foal2-1.fna&oh=00_AfBEDErV8YH9l9rvpJVBrkkoqqEzPRjfvMnZNY_CcBkAvQ&oe=642C06A8"
       perfilUrl="https://shayarimaza.com/files/boys-dp-images/sad-boy-dp-images/Sad-boy-Profile-Pic.jpg"
-      name="Artur Alexandre Calderon"
-      profissao="Programador"
-      aboutME="Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis officiis molestias accusamus iure ad, amet voluptatum magni tempora repudiandae praesentium molestiae adipisci, deserunt nemo commodi eum aliquid voluptatem. Soluta, aliquam"
+      name="Insta Kit"
+      profissao="Influencers, Empresas e Organizações"
+      aboutME="Diga um pouco sobre você, para seus fãs, clientes e seguidores. Reúna seus principais links em um só lugar. Mude o layout do seu jeito. Quer ter um igual? Entre em contato nos links abaixo."
       instaLink="https://www.instagram.com/artur.jsx/"
+      whatsLink='https://wa.me/5569999462554?text=Ola%2C+vim+pelo+instaKit+quero+um+igual!'
     />
   )
 }
